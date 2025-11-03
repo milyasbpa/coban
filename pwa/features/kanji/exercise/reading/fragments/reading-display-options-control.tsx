d049cebdf1b586e94fe4f65b4c32a4ad781e5627
milyasbpa <basilyas.2015@gmail.com>
2025-11-03 22:45:50 +0700

    fix: toggle

--- pwa/features/kanji/exercise/reading/fragments/reading-display-options-control.tsx
+++ pwa/features/kanji/exercise/reading/fragments/reading-display-options-control.tsx
@@ -30,7 +30,7 @@ export function ReadingDisplayOptionsControl() {
   return (
     <>
       {/* Floating Button */}
-      <div className="fixed bottom-6 right-6 z-50">
+      <div className="fixed bottom-30 right-6 z-50">
         <Button
           variant="default"
           size="icon"
