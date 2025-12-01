6b8df50d3e9fa85cffd9950effb23f2ca611d42f
milyasbpa <basilyas.2015@gmail.com>
2025-12-01 09:14:39 +0700

    fix: error build

--- pwa/features/kanji/exercise/pairing/fragments/index.ts
+++ pwa/features/kanji/exercise/pairing/fragments/index.ts
@@ -1,5 +1,4 @@
-export { PairingScoreHeader } from './pairing-score-header';
-export { PairingGameResult } from './pairing-game-result';
-export { PairingHeader } from './pairing-header';
-export { PairingGameGrid } from './pairing-game-grid';
-export { PairingDisplayOptionsControl } from './pairing-display-options-control';
+export { PairingScoreHeader } from "./pairing-score-header";
+export { PairingGameResult } from "./pairing-game-result";
+export { PairingHeader } from "./pairing-header";
+export { PairingGameGrid } from "./pairing-game-grid";
